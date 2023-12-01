c058c4dc8abea802a0239c0f8ab7d6eb672edfa8
Malcomn <malcomn.j.mitchell@gmail.com>
2023-12-01 10:37:06 -0500

    Finished Day One

---
+++ AdventOfCodeDayOne/Program.cs
@@ -0,0 +1,35 @@
+﻿// Day 1: Trebuchet?!
+const string filePath = "calibration_document.txt";
+var lines = File.ReadAllLines(filePath).ToList();
+
+static int DecodeCalibrationDocument(List<string> calibrationDocumentLines)
+{
+    if (calibrationDocumentLines?.Any() != true)
+    {
+        throw new ArgumentException("Calibration document lines are null or empty.");
+    }
+    
+    var numbers = new List<int>();
+    foreach (var line in calibrationDocumentLines)
+    {
+        var digits = line.Where(char.IsDigit).ToList();
+        if (digits.Count == 0) continue;
+        
+        var firstDigit = digits.First();
+        var lastDigit = digits.Last();
+        var number = int.Parse($"{firstDigit}{lastDigit}");
+        numbers.Add(number);
+    }
+
+    return numbers.Sum();
+}
+
+try
+{
+    var calibrationValue = DecodeCalibrationDocument(lines);
+    Console.WriteLine(calibrationValue);
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
